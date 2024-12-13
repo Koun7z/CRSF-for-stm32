0965637861db91c69db353c8b6b89ee2a6f5009e
Piotr Woliński <pwolinski@student.agh.edu.pl>
2024-12-13 19:12:00 +0100

    Init

---
+++ Core/Inc/CRSF_Connection.h
@@ -0,0 +1,205 @@
+/*
+ * CRSF_Connection.h
+ *
+ *  Created on: Dec 7, 2024
+ *      Author: pwoli
+ */
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+#ifndef INC_CRSF_CONNECTION_H_
+#define INC_CRSF_CONNECTION_H_
+
+UART_HandleTypeDef* _uart;
+CRC_HandleTypeDef* _crc;
+
+uint8_t CRSF_Buffer[64];
+
+uint32_t CRSF_LastChannelsPacked = 0;
+struct CRSF_ChannelsPacked CRSF_Channels;
+
+bool CRSF_NewData = false;
+
+#define CRSF_SYNC_BYTE CRSF_Buffer[0]
+#define CRSF_MSG_LEN CRSF_Buffer[1]
+#define CRSF_FRAME_TYPE CRSF_Buffer[2]
+#define CRSF_DATA_BEGIN &(CRSF_Buffer[3])
+#define CRSF_DATA_LEN (CRSF_MSG_LEN - 2)
+#define CRSF_CRC_BEGIN &(CRSF_Buffer[2])
+
+typedef enum
+{
+	CRSF_SYNC_DEFAULT   = 0xC8,
+	CRSF_SYNC_EDGE_TX   = 0xEE
+} CRSF_SYNC;
+
+typedef enum
+{
+	CRSF_FRAMETYPE_GPS 					     = 0x02, // GPS position, ground speed, heading, altitude, satellite count
+	CRSF_FRAMETYPE_VARIO 			     	 = 0x07, // Vertical speed
+	CRSF_FRAMETYPE_BATTERY_SENSOR 		     = 0x08, // Battery voltage, current, mAh, remaining percent
+	CRSF_FRAMETYPE_BARO_ALTITUDE 			 = 0x09, // Barometric altitude, vertical speed (optional)
+	CRSF_FRAMETYPE_HEARTBEAT 				 = 0x0B, // (CRSFv3) Heartbeat
+	CRSF_FRAMETYPE_LINK_STATISTICS 		  	 = 0x14, // Signal information. Uplink/Downlink RSSI, SNR, Link Quality (LQ), RF mode, transmit power
+	CRSF_FRAMETYPE_RC_CHANNELS_PACKED 		 = 0x16, // Channels data (both handset to TX and RX to flight controller)
+	CRSF_FRAMETYPE_SUBSET_RC_CHANNELS_PACKED = 0x17, // (CRSFv3) Channels subset data
+	CRSF_FRAMETYPE_LINK_RX_ID 				 = 0x1C, // Receiver RSSI percent, power?
+	CRSF_FRAMETYPE_LINK_TX_ID 			     = 0x1D, // Transmitter RSSI percent, power, fps?
+	CRSF_FRAMETYPE_ATTITUDE 			     = 0x1E, // Attitude: pitch, roll, yaw
+	CRSF_FRAMETYPE_FLIGHT_MODE 			     = 0x21, // Flight controller flight mode string
+	CRSF_FRAMETYPE_DEVICE_PING 			     = 0x28, // Sender requesting DEVICE_INFO from all destination devices
+	CRSF_FRAMETYPE_DEVICE_INFO 		         = 0x29, // Device name, firmware version, hardware version, serial number (PING response)
+	CRSF_FRAMETYPE_PARAMETER_SETTINGS_ENTRY  = 0x2B, // Configuration item data chunk
+	CRSF_FRAMETYPE_PARAMETER_READ 			 = 0x2C, // Configuration item read request
+	CRSF_FRAMETYPE_PARAMETER_WRITE 		     = 0x2D, // Configuration item write request
+	CRSF_FRAMETYPE_ELRS_STATUS 			     = 0x2E, // !!Non Standard!! ExpressLRS good/bad packet count, status flags
+	CRSF_FRAMETYPE_COMMAND 				     = 0x32, // CRSF command execute
+	CRSF_FRAMETYPE_RADIO_ID 				 = 0x3A, // Extended type used for OPENTX_SYNC
+	CRSF_FRAMETYPE_KISS_REQ 				 = 0x78, // KISS request
+	CRSF_FRAMETYPE_KISS_RESP 				 = 0x79, // KISS response
+	CRSF_FRAMETYPE_MSP_REQ 				     = 0x7A, // MSP parameter request / command
+	CRSF_FRAMETYPE_MSP_RESP 				 = 0x7B, // MSP parameter response chunk
+	CRSF_FRAMETYPE_MSP_WRITE 				 = 0x7C, // MSP parameter write
+	CRSF_FRAMETYPE_DISPLAYPORT_CMD 		     = 0x7D, // (CRSFv3) MSP DisplayPort control command
+	CRSF_FRAMETYPE_ARDUPILOT_RESP 			 = 0x80  // Ardupilot output?
+} CRSF_FRAMETYPE;
+
+typedef enum
+{
+	CRSF_ADDRESS_BROADCAST 		   = 0x00, // Broadcast (all devices process packet)
+	CRSF_ADDRESS_USB 			   = 0x10, // ?
+	CRSF_ADDRESS_BLUETOOTH 		   = 0x12, // Bluetooth module
+	CRSF_ADDRESS_TBS_CORE_PNP_PRO   = 0x80, // ?
+	CRSF_ADDRESS_RESERVED1		   = 0x8A, // Reserved for one
+	CRSF_ADDRESS_SENSOR    		   = 0xC0, // External current sensor
+	CRSF_ADDRESS_GPS 			   = 0xC2, // External GPS
+	CRSF_ADDRESS_TBS_BLACKBOX 	   = 0xC4, // External Blackbox logging device
+	CRSF_ADDRESS_FLIGHT_CONTROLLER  = 0xC8, // Flight Controller (Betaflight / iNav)
+	CRSF_ADDRESS_RESERVED2 		   = 0xCA, // Reserved, for two
+	CRSF_ADDRESS_RACE_TAG 		   = 0xCC, // Race tag?
+	CRSF_ADDRESS_RADIO_TRANSMITTER = 0xEA, // Handset (EdgeTX), not transmitter
+	CRSF_ADDRESS_CRSF_RECEIVER 	   = 0xEC, // Receiver hardware (TBS Nano RX / RadioMaster RP1)
+	CRSF_ADDRESS_CRSF_TRANSMITTER  = 0xEE, // Transmitter module, not handset
+	CRSF_ADDRESS_ELRS_LUA 		   = 0xEF  // !!Non-Standard!! Source address used by ExpressLRS Lua
+} CRSF_ADDRESS;
+
+struct __attribute__((packed))CRSF_ChannelsPacked
+{
+    unsigned ch0 : 11;
+    unsigned ch1 : 11;
+    unsigned ch2 : 11;
+    unsigned ch3 : 11;
+    unsigned ch4 : 11;
+    unsigned ch5 : 11;
+    unsigned ch6 : 11;
+    unsigned ch7 : 11;
+    unsigned ch8 : 11;
+    unsigned ch9 : 11;
+    unsigned ch10 : 11;
+    unsigned ch11 : 11;
+    unsigned ch12 : 11;
+    unsigned ch13 : 11;
+    unsigned ch14 : 11;
+    unsigned ch15 : 11;
+    uint8_t armStatus; // optional ExpressLRS 4.0
+};
+
+
+static void _receptionComplete()
+{
+	CRSF_SYNC_BYTE = 0;
+	CRSF_MSG_LEN = 0;
+	CRSF_NewData = false;
+
+	HAL_UARTEx_ReceiveToIdle_DMA(_uart, CRSF_Buffer, 64);
+	__HAL_DMA_DISABLE_IT(_uart->hdmarx, DMA_IT_HT);
+}
+
+static void _parseData()
+{
+	switch(CRSF_FRAME_TYPE)
+	{
+		case CRSF_FRAMETYPE_HEARTBEAT:
+		case CRSF_FRAMETYPE_LINK_STATISTICS:
+			printf("TODO: %u\n", CRSF_FRAME_TYPE);
+			break;
+
+		case CRSF_FRAMETYPE_RC_CHANNELS_PACKED:
+			memcpy(&CRSF_Channels, CRSF_DATA_BEGIN, CRSF_DATA_LEN);
+			CRSF_LastChannelsPacked = HAL_GetTick();
+			break;
+
+		case CRSF_FRAMETYPE_SUBSET_RC_CHANNELS_PACKED:
+		case CRSF_FRAMETYPE_DEVICE_PING:
+		case CRSF_FRAMETYPE_DEVICE_INFO:
+		case CRSF_FRAMETYPE_PARAMETER_SETTINGS_ENTRY:
+		case CRSF_FRAMETYPE_PARAMETER_READ:
+		case CRSF_FRAMETYPE_PARAMETER_WRITE:
+		case CRSF_FRAMETYPE_ELRS_STATUS:
+		case CRSF_FRAMETYPE_COMMAND:
+		case CRSF_FRAMETYPE_RADIO_ID:
+		case CRSF_FRAMETYPE_KISS_REQ:
+		case CRSF_FRAMETYPE_KISS_RESP:
+		case CRSF_FRAMETYPE_MSP_REQ:
+		case CRSF_FRAMETYPE_MSP_RESP:
+		case CRSF_FRAMETYPE_MSP_WRITE:
+		case CRSF_FRAMETYPE_DISPLAYPORT_CMD:
+		case CRSF_FRAMETYPE_ARDUPILOT_RESP:
+			printf("TODO: %u\n", CRSF_FRAME_TYPE);
+			break;
+
+		default:
+			printf("Unhandled pc %u\n", CRSF_FRAME_TYPE);
+			break;
+	}
+}
+
+void CRSF_HandleRX(UART_HandleTypeDef *uart)
+{
+	if(uart != _uart)
+	{
+		return;
+	}
+
+	if(CRSF_SYNC_BYTE != CRSF_SYNC_DEFAULT && CRSF_SYNC_BYTE != CRSF_SYNC_EDGE_TX)
+	{
+		HAL_UARTEx_ReceiveToIdle_DMA(_uart, CRSF_Buffer, 64);
+		__HAL_DMA_DISABLE_IT(_uart->hdmarx, DMA_IT_HT);
+		return;
+	}
+
+	if(HAL_CRC_Calculate(_crc, CRSF_CRC_BEGIN, CRSF_MSG_LEN) != 0)
+	{
+		printf("CRC-err\n");
+		_receptionComplete();
+		return;
+	}
+
+	_parseData();
+
+	_receptionComplete();
+}
+
+void CRSF_HandleErr(UART_HandleTypeDef* huart)
+{
+	if(huart != _uart)
+	{
+		return;
+	}
+
+	uint32_t err = huart->ErrorCode;;
+	printf("err: %lu\n", err);
+
+	_receptionComplete();
+}
+
+void CRSF_Init(UART_HandleTypeDef* huart, CRC_HandleTypeDef* hcrc)
+{
+	_uart = huart;
+	_crc = hcrc;
+	_receptionComplete();
+}
+
+#endif /* INC_CRSF_CONNECTION_H_ */
---
+++ Core/Src/CRSF_Connection.c
@@ -0,0 +1,9 @@
+/*
+ * CRSF_Connection.c
+ *
+ *  Created on: Dec 13, 2024
+ *      Author: pwoli
+ */
+
+
+#include "CRSF_Connection.h"
